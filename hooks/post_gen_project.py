256736016584d889f43b029a164d60244696e9a5
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-02-23 15:09:40 +0100

    remove model.xml if another model was given
    * do not require an absolute path
    * we don't want setup.py in the project

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,9 +2,10 @@ import os
 import shutil
 from os.path import basename
 
-model_path = '{{ cookiecutter.model }}'
-if model_path != 'default':
-    if not os.path.isabs(model_path):
-        raise ValueError('You need to provide an absolute path to the model.')
-    else:
+model_path = "{{ cookiecutter.model }}"
+if model_path != "default":
+#    if not os.path.isabs(model_path):
+#        raise ValueError('You need to provide an absolute path to the model.')
+#    else:
         shutil.copyfile(model_path, basename(model_path))
+        os.remove("model.xml")
