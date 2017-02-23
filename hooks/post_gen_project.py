eb104ff1cd2bd9155291f9eaef6758070dadee90
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-02-23 11:52:27 +0100

    clean up weird testing code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,13 +2,6 @@ import os
 import shutil
 from os.path import basename
 
-# from cookiecutter import config
-# print(config.DEFAULT_CONFIG)
-# if os.path.isabs('{{ cookiecutter.model }}'):
-#     model_path = '{{ cookiecutter.model }}'
-# else:
-#     print(1, '{{ cookiecutter.vanilla }}')
-#     model_path = '{{ cookiecutter.repo_dir }}{{ cookiecutter.model }}'
 model_path = '{{ cookiecutter.model }}'
 if model_path != 'default':
     if not os.path.isabs(model_path):
--- tests/test_create_project.sh
+++ tests/test_create_project.sh
@@ -2,17 +2,13 @@
 
 set -e
 
-require() {
-    type $1 >/dev/null 2>/dev/null
-}
-
 cleanup() {
-    rm -rf python_boilerplate
+    rm -rf memote_model_repository
 }
 trap cleanup EXIT
 
 
-require cookiecutter
+type cookiecutter
 
 echo "Running test script..."
 cookiecutter . --no-input
