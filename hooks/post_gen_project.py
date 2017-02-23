8620a01afdadfb7a17cc77a4e7d995af63c360f4
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-02-23 18:55:40 +0100

    include model path into tox.ini

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import os
 import shutil
-from os.path import basename
+from os.path import basename, isabs
 
 model_path = "{{ cookiecutter.model }}"
-if model_path != "default":
+if model_path != "model.xml":
+    if not isabs(model_path):
+        raise ValueError("You need to provide an absolute path to the model.")
     shutil.copyfile(model_path, basename(model_path))
     os.remove("model.xml")
