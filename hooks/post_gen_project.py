acde14e0ccd941bc5750daf184ce7af15e11e68a
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-02-24 13:03:20 +0100

    add logging info

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import logging
 import os
 import shutil
 from os.path import basename, isabs
+
+LOGGER = logging.getLogger(__name__)
+
 
 model_path = "{{ cookiecutter.model }}"
 if model_path != "model.xml":
     if not isabs(model_path):
         raise ValueError("You need to provide an absolute path to the model.")
-    shutil.copyfile(model_path, basename(model_path))
+    target = basename(model_path)
+    LOGGER.info("copying model '%s' -> '%s'", model_path, target)
+    shutil.copyfile(model_path, target)
     os.remove("model.xml")
