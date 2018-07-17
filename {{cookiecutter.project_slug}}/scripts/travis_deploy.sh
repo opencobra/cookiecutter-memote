3c614251c41e843e01966e310d45c44972527a95
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-07-17 12:31:11 +0200

    refactor: make repo setup more robust

--- {{cookiecutter.project_slug}}/scripts/travis_deploy.sh
+++ {{cookiecutter.project_slug}}/scripts/travis_deploy.sh
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 
-# Do NOT set -v or your GitHub API token will be leaked!
+# Do NOT set -v or -x or your GitHub API token will be leaked!
 set -ue # exit with nonzero exit code if anything fails
 
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" || "${TRAVIS_REPO_SLUG}" != "{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}" ]]; then
-    echo "Skip deploy."
-    exit 0
-else
-  echo "Start deploy to ${DEPLOY_BRANCH}..."
-fi
+echo "Parse memote.ini for values."
+deployment=$(awk -F '=' '{if (! ($0 ~ /^;/) && $0 ~ /deployment/) print $2}' memote.ini | tr -d ' ')
+location=$(awk -F '=' '{if (! ($0 ~ /^;/) && $0 ~ /location/) print $2}' memote.ini | tr -d ' ')
 
-# configure git
+echo "Configure Travis git user."
 git config --global user.email "deploy@travis-ci.org"
 git config --global user.name "Travis CI Deployment Bot"
 
-# clone the deploy branch
-cd "/tmp"
-git clone --quiet --branch=${DEPLOY_BRANCH} https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git ${DEPLOY_BRANCH} > /dev/null
-deploy_dir="/tmp/${DEPLOY_BRANCH}"
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" || "${TRAVIS_REPO_SLUG}" != "{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}" ]]; then
+    echo "Untracked build."
+    memote run --ignore-git
+    echo "Skip deploy."
+    exit 0
+else
+    # Always need the deployment branch available locally for storing results.
+    git checkout "${deployment}"
+    git checkout "${TRAVIS_BRANCH}"
+    echo "Tracked build."
+    memote run
+    echo "Start deploy to ${deployment}..."
+fi
 
-# copy the results from the current memote run to deploy dir
-cp "${TRAVIS_BUILD_DIR}/Results/${TRAVIS_COMMIT}.json" "${deploy_dir}/Results/"
+# Generate the history report on the deployment branch.
+output="index.html"
+git checkout "${deployment}"
+echo "Generating updated history report '${output}'."
+memote report history --filename="${output}"
 
-# create the report pointing to the history stored in deploy branch
-# need to be in build directory to access git history
-cd "${TRAVIS_BUILD_DIR}"
-memote report history --filename="${deploy_dir}/index.html" "${deploy_dir}/Results/"
+# Add, commit and push the files.
+git add "${output}"
+git commit -m "Travis report #${TRAVIS_BUILD_NUMBER}"
+git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${deployment}" > /dev/null
 
-#add, commit and push files
-cd "${deploy_dir}"
-git add .
-git commit -m "Travis build ${TRAVIS_BUILD_NUMBER}"
-git push --quiet origin "${DEPLOY_BRANCH}" > /dev/null
-
-echo "Done."
+echo "Your new report will be visible at https://{{ cookiecutter.github_username }}.github.io/{{ cookiecutter.project_slug }} in a moment."
